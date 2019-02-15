23a9408dc0bd3782711c3c58d763f0e6f14edcd9
mkazhdan <misha@cs.jhu.edu>
2019-02-15 08:30:37 -0500

    no message

--- Src/ConvertPLY.cpp
+++ Src/ConvertPLY.cpp
@@ -34,6 +34,7 @@ DAMAGE.
 #include <float.h>
 #include <algorithm>
 #include <limits>
+#include <unordered_map>
 #include "FEMTree.h"
 #include "MyMiscellany.h"
 #include "CmdLineParser.h"
@@ -43,28 +44,62 @@ DAMAGE.
 #include "PointStreamData.h"
 
 cmdLineParameter< char* > In( "in" ) , Out( "out" );
+cmdLineParameterArray< float , 2*DIMENSION > BoundingBox( "bBox" );
+cmdLineReadable ASCII( "ascii" );
 
-cmdLineReadable* params[] = { &In , &Out , NULL };
+cmdLineReadable* params[] = { &In , &Out , &BoundingBox , &ASCII , NULL };
 
 void ShowUsage( char* ex )
 {
 	printf( "Usage: %s\n" , ex );
 	printf( "\t --%s <input polygon mesh>\n" , In.name );
 	printf( "\t[--%s <ouput polygon mesh>]\n" , Out.name );
+	printf( "\t[--%s <bounding box>]\n" , BoundingBox.name );
+	printf( "\t[--%s]\n" , ASCII.name );
 }
 
+template< typename ... VertexData >
+void WriteMesh( const char *fileName , int ft , const std::vector< PlyVertexWithData< float , DIMENSION , MultiPointStreamData< float , VertexData ... > > > &vertices , const std::vector< std::vector< long long > > &polygons , const std::vector< std::string > &comments )
+{
+	typedef PlyVertexWithData< float , DIMENSION , MultiPointStreamData< float , VertexData ... > > Vertex;
+	if( vertices.size()>std::numeric_limits< int >::max() )
+	{
+		if( vertices.size()>std::numeric_limits< unsigned int >::max() ) ERROR_OUT( "more vertices than can be indexed by an unsigned int: %llu" , (unsigned long long)vertices.size() );
+		WARN( "more vertices than can be indexed by an int, using unsigned int instead: %llu" , (unsigned long long)vertices.size() );
+		std::vector< std::vector< unsigned int > > outPolygons;
+		outPolygons.resize( polygons.size() );
+		for( size_t i=0 ; i<polygons.size() ; i++ )
+		{
+			outPolygons[i].resize( polygons[i].size() );
+			for( int j=0 ; j<polygons[i].size() ; j++ ) outPolygons[i][j] = (unsigned int)polygons[i][j];
+		}
+		if( !PlyWritePolygons< Vertex >( fileName , vertices , outPolygons , Vertex::PlyWriteProperties() , Vertex::PlyWriteNum , ft , comments ) ) ERROR_OUT( "Could not write mesh to: " , fileName );
+	}
+	else
+	{
+		std::vector< std::vector< int > > outPolygons;
+		outPolygons.resize( polygons.size() );
+		for( size_t i=0 ; i<polygons.size() ; i++ )
+		{
+			outPolygons[i].resize( polygons[i].size() );
+			for( int j=0 ; j<polygons[i].size() ; j++ ) outPolygons[i][j] = (int)polygons[i][j];
+		}
+		if( !PlyWritePolygons< Vertex >( fileName , vertices , outPolygons , Vertex::PlyWriteProperties() , Vertex::PlyWriteNum , ft , comments ) ) ERROR_OUT( "Could not write mesh to: " , fileName );
+	}
+
+}
 
 template< typename ... VertexData >
 int Execute( void )
 {
 	typedef PlyVertexWithData< float , DIMENSION , MultiPointStreamData< float , VertexData ... > > Vertex;
 	std::vector< Vertex > vertices;
-	std::vector< std::vector< long long > > inPolygons;
+	std::vector< std::vector< long long > > polygons;
 
 	int ft;
 	std::vector< std::string > comments;
-	PlyReadPolygons< Vertex >( In.value , vertices , inPolygons , Vertex::PlyReadProperties() , Vertex::PlyReadNum , ft , comments );
-	printf( "Vertices / Polygons: %llu / %llu\n" , (unsigned long long)vertices.size() , (unsigned long long)inPolygons.size() );
+	PlyReadPolygons< Vertex >( In.value , vertices , polygons , Vertex::PlyReadProperties() , Vertex::PlyReadNum , ft , comments );
+	printf( "Vertices / Polygons: %llu / %llu\n" , (unsigned long long)vertices.size() , (unsigned long long)polygons.size() );
 
 	Point< float , DIMENSION > min , max;
 	min = max = vertices[0].point;
@@ -79,33 +114,41 @@ int Execute( void )
 	for( unsigned int d=0 ; d<DIMENSION ; d++ ) printf( " %f" , max[d] );
 	printf( " ]\n" );
 
-	if( Out.set )
+	if( BoundingBox.set )
 	{
-		if( vertices.size()>std::numeric_limits< int >::max() )
+		std::vector< std::vector< long long > > subPolygons;
+		for( size_t i=0 ; i<polygons.size() ; i++ )
 		{
-			if( vertices.size()>std::numeric_limits< unsigned int >::max() ) ERROR_OUT( "more vertices than can be indexed by an unsigned int: %llu" , (unsigned long long)vertices.size() );
-			WARN( "more vertices than can be indexed by an int, using unsigned int instead: %llu" , (unsigned long long)vertices.size() );
-			std::vector< std::vector< unsigned int > > outPolygons;
-			outPolygons.resize( inPolygons.size() );
-			for( size_t i=0 ; i<inPolygons.size() ; i++ )
-			{
-				outPolygons[i].resize( inPolygons[i].size() );
-				for( int j=0 ; j<inPolygons[i].size() ; j++ ) outPolygons[i][j] = (unsigned int)inPolygons[i][j];
-			}
-			if( !PlyWritePolygons< Vertex >( Out.value , vertices , outPolygons , Vertex::PlyWriteProperties() , Vertex::PlyWriteNum , ft , comments ) ) ERROR_OUT( "Could not write mesh to: " , Out.value );
+			Point< float , DIMENSION > center;
+			for( size_t j=0 ; j<polygons[i].size() ; j++ ) center += vertices[ polygons[i][j] ].point;
+			center /= (float)polygons[i].size();
+			bool inside = true;
+			for( unsigned int d=0 ; d<DIMENSION ; d++ ) if( center[d]<BoundingBox.values[d] || center[d]>=BoundingBox.values[d+DIMENSION] ) inside = false;
+			if( inside ) subPolygons.push_back( polygons[i] );
 		}
-		else
+
+		long long count = 0;
+		std::unordered_map< long long , long long > vMap;
+		for( size_t i=0 ; i<polygons.size() ; i++ ) for( size_t j=0 ; j<polygons[i].size() ; j++ )
 		{
-			std::vector< std::vector< int > > outPolygons;
-			outPolygons.resize( inPolygons.size() );
-			for( size_t i=0 ; i<inPolygons.size() ; i++ )
-			{
-				outPolygons[i].resize( inPolygons[i].size() );
-				for( int j=0 ; j<inPolygons[i].size() ; j++ ) outPolygons[i][j] = (int)inPolygons[i][j];
-			}
-			if( !PlyWritePolygons< Vertex >( Out.value , vertices , outPolygons , Vertex::PlyWriteProperties() , Vertex::PlyWriteNum , ft , comments ) ) ERROR_OUT( "Could not write mesh to: " , Out.value );
+			auto iter = vMap.find( polygons[i][j] );
+			if( iter==vMap.end() ) vMap[ polygons[i][j] ] = count++;
 		}
+		std::vector< Vertex > subVertices( vMap.size() );
+		for( size_t i=0 ; i<subPolygons.size() ; i++ ) for( size_t j=0 ; j<subPolygons[i].size() ; j++ )
+		{
+			long long oldIdx = polygons[i][j];
+			long long newIdx = vMap[ oldIdx ];
+			subVertices[ newIdx ] = vertices[ oldIdx ];
+			subPolygons[i][j] = newIdx;
+		}
+		if( Out.set ) WriteMesh( Out.value , ASCII.set ? PLY_ASCII : ft , subVertices , subPolygons , comments );
 	}
+	else
+	{
+		if( Out.set ) WriteMesh( Out.value , ASCII.set ? PLY_ASCII : ft , vertices , polygons , comments );
+	}
+
 	return EXIT_SUCCESS;
 }
 int main( int argc , char* argv[] )
