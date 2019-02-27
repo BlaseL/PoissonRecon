488a0ac38d6ed3a3ab3d3cfe5ee8aad569957345
mkazhdan <misha@cs.jhu.edu>
2019-02-27 10:48:06 -0500

    no message

--- Src/ConvertPLY.cpp
+++ Src/ConvertPLY.cpp
@@ -26,7 +26,7 @@ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF S
 DAMAGE.
 */
 #define NEW_CODE
-#define NEW_CHUNKS
+#undef NEW_CHUNKS
 
 #include <stdio.h>
 #include <stdlib.h>
